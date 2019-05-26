75d6d6f784945afbae4304b26c865914111d1b9a
xiayx <xiayouxue@hotmail.com>
2019-05-26 13:09:51 +0800

    完善javadocs

--- peacetrue-modelgenerator-template-aum/src/main/java/com/github/peacetrue/modelgenerator/template/aum/ModelGeneratorTemplateAumAutoConfiguration.java
+++ peacetrue-modelgenerator-template-aum/src/main/java/com/github/peacetrue/modelgenerator/template/aum/ModelGeneratorTemplateAumAutoConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**
+ * 黄金管家模板配置
+ *
  * @author xiayx
  */
 @Configuration
